024da69e7b21167e8a6d8e7b0889f9bf1f92d68e
ornecurcio <ornecurcio@gmail.com>
2021-09-19 01:59:51 -0300

    detalles?

--- TP1/Entidades/Calculadora.cs
+++ TP1/Entidades/Calculadora.cs
@@ -4,6 +4,11 @@ namespace Entidades
 {
     public static class Calculadora
     {
+        /// <summary>
+        /// Valida que sea un operador matematico
+        /// </summary>
+        /// <param name="operador">a validar</param>
+        /// <returns>operador validado o default +</returns>
         private static char ValidarOperador(char operador)
         {
             if (operador == '+' || operador == '-' || operador == '*' || operador == '/')
@@ -12,26 +17,33 @@ namespace Entidades
             }
             return '+';
         }
+        /// <summary>
+        /// Realiza la operacion matematica
+        /// </summary>
+        /// <param name="num1">tipo operando</param>
+        /// <param name="num2">tipo operando</param>
+        /// <param name="operador">operador matematico</param>
+        /// <returns>resultado de operacion matematica en double</returns>
         public static double Operar(Operando num1, Operando num2, char operador)
         {
-            double rta = 0;
+            double resultado = 0;
             operador = ValidarOperador(operador);
             switch (operador)
             {
                 case '+':
-                    rta = num1 + num2;
+                    resultado = num1 + num2;
                     break;
                 case '-':
-                    rta = num1 - num2;
+                    resultado = num1 - num2;
                     break;
                 case '/':
-                    rta = num1 / num2;
+                    resultado = num1 / num2;
                     break;
                 case '*':
-                    rta = num1 * num2;
+                    resultado = num1 * num2;
                     break;
             }
-            return rta;
+            return resultado;
         }
     }
 }
--- TP1/Entidades/Operando.cs
+++ TP1/Entidades/Operando.cs
@@ -8,7 +8,9 @@ namespace Entidades
 {
     public class Operando
     {
+        #region Atributos
         private double numero;
+        #endregion
         #region Propiedades
         private string Numero
         {
@@ -19,18 +21,34 @@ namespace Entidades
         }
         #endregion
         #region Constructores
+        /// <summary>
+        /// Constructor por defecto, inicializa en 0
+        /// </summary>
         public Operando() : this(0)
         { }
+        /// <summary>
+        /// Sobrecarga constructor con double
+        /// </summary>
+        /// <param name="numero"></param>
         public Operando(double numero)
         {
             this.numero = numero;
         }
+        /// <summary>
+        /// Sobrecarga constructor con string
+        /// </summary>
+        /// <param name="strNumero"></param>
         public Operando(string strNumero)
         {
             this.Numero = strNumero;
         }
         #endregion
         #region Validaciones
+        /// <summary>
+        /// Valida que el string recibido sea un numero
+        /// </summary>
+        /// <param name="strNumero"></param>
+        /// <returns>double si string valido, 0 si string no valido</returns>
         private double ValidarOperando(string strNumero)
         {
             if (Double.TryParse(strNumero, out double operando))
@@ -39,9 +57,14 @@ namespace Entidades
             }
             return 0;
         }
+        /// <summary>
+        /// Valida si string recibido compuesto de 0 o 1
+        /// </summary>
+        /// <param name="binario"></param>
+        /// <returns>true si correcto, false si incorrecto</returns>
         private bool EsBinario(string binario)
         {
-            if (!String.IsNullOrEmpty(binario))
+            if (!string.IsNullOrWhiteSpace(binario))
             {
                 foreach (char item in binario)
                 {
@@ -57,14 +80,32 @@ namespace Entidades
         }
         #endregion
         #region Operadores
+        /// <summary>
+        /// Sobrecarga de operador -
+        /// </summary>
+        /// <param name="n1">tipo Operando</param>
+        /// <param name="n2">tipo Operando</param>
+        /// <returns>valor de operacion en double</returns>
         public static double operator -(Operando n1, Operando n2)
         {
            return (n1.numero-n2.numero); 
         }
+        /// <summary>
+        /// Sobrecarga de operador *
+        /// </summary>
+        /// <param name="n1">tipo Operando</param>
+        /// <param name="n2">tipo Operando</param>
+        /// <returns>valor de operacion en double</returns>
         public static double operator *(Operando n1, Operando n2)
         {
             return (n1.numero * n2.numero);
         }
+        /// <summary>
+        /// Sobrecarga de operador /
+        /// </summary>
+        /// <param name="n1">tipo Operando</param>
+        /// <param name="n2">tipo Operando</param>
+        /// <returns>valor de operacion en double, si divisor 0 valor minimo</returns>
         public static double operator /(Operando n1, Operando n2)
         {
             if (n2.numero != 0)
@@ -73,15 +114,26 @@ namespace Entidades
             }
             return Double.MinValue;
         }
+        /// <summary>
+        /// Sobrecarga de operador +
+        /// </summary>
+        /// <param name="n1">tipo Operando</param>
+        /// <param name="n2">tipo Operando</param>
+        /// <returns>valor de operacion en double</returns>
         public static double operator +(Operando n1, Operando n2)
         {
            return n1.numero + n2.numero;
         }
         #endregion
         #region Metodos
+        /// <summary>
+        /// Conversor de Binario a Decimal
+        /// </summary>
+        /// <param name="binario">tipo string</param>
+        /// <returns>numero decimal convertido tipo string</returns>
         public string BinarioDecimal(string binario)
         {
-            if (!String.IsNullOrEmpty(binario) && EsBinario(binario))
+            if (!String.IsNullOrWhiteSpace(binario) && EsBinario(binario))
             {
                 double auxNum = 0;
                 double auxPotencias = binario.Length - 1;                    
@@ -97,6 +149,11 @@ namespace Entidades
             }
             return "Valor Invalido";
         }
+        /// <summary>
+        /// Conversor de Decimal a Binario 
+        /// </summary>
+        /// <param name="numero">tipo double</param>
+        /// <returns>binario en string</returns>
         public string DecimalBinario(double numero) 
         {
             string binarioStr = string.Empty;
@@ -112,6 +169,11 @@ namespace Entidades
             }
             return binarioStr;
         }
+        /// <summary>
+        /// Conversor Decimal a Binario
+        /// </summary>uso interno de DecimalBinario numerico
+        /// <param name="numeroStr">numero recivido como string</param>
+        /// <returns>Binario de tipo string</returns>
         public string DecimalBinario(string numeroStr)
         {
             if (Double.TryParse(numeroStr, out double numDecimal))
@@ -122,5 +184,4 @@ namespace Entidades
         }
         #endregion
     }
-
 }
